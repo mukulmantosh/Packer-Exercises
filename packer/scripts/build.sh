ae2590261e4d387442e2f26ca064e9efe766ec94
Mukul Mantosh <mukulmantosh@yahoo.com>
2022-11-26 08:45:22 +0530

    Feat: - Initial Commit + FastAPI Code

--- packer/scripts/build.sh
+++ packer/scripts/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+sudo yum install jq -y
+sudo yum install -y git
+
+sudo yum install -y docker
+sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker.service
+
 sudo amazon-linux-extras install nginx1 -y
 sudo systemctl enable nginx.service
 sudo systemctl start nginx.service
+
+IMAGE_TAG=`curl -L -s 'https://hub.docker.com/v2/repositories/mukulmantosh/packerexercise/tags'|jq '."results"[0]["name"]' | bc`
+
+sudo docker pull mukulmantosh/packerexercise:$IMAGE_TAG
+sudo docker run -d --name fastapi -p 8080:8080 mukulmantosh/packerexercise:$IMAGE_TAG
